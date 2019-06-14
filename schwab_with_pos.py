44509c145fd4188410f104079a0df4da9b5837e1
joanarau-schweizer <joan.arau@me.com>
2019-06-14 21:38:05 +0100

    /

--- schwab_with_pos.py
+++ schwab_with_pos.py
@@ -3,25 +3,22 @@ from   selenium import webdriver
 from   selenium.webdriver.common.by import By
 import time
 import datetime
-now = datetime.date.today().strftime(" %B %d %Y")
+now = datetime.date.today().strftime('%Y-%m-%d')
+print(now)
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import os
 import glob
 import shutil
-path = config.db_path
+import pandas as pd
+path = os.path.join(config.db_path,'schwab')
 LOGIN = config.schwab_cred['login']
 PASSWORD = config.schwab_cred['password']
-
+pd.set_option('display.max_columns', 500)
 def get_balance():
     options = Options()
 
-
-    # options.add_argument("headless")
-    # options.add_argument("--disable-gpu")
-    # options.add_argument("--window-size=600,800")
-
-    browser = webdriver.Chrome(executable_path=r'/Users/joanarau-schweizer/PycharmProjects/stockbot/chromedriver 10.15.44 pm',options=options)
+    browser = webdriver.Chrome(executable_path=r'/Users/joanarau-schweizer/PycharmProjects/stockbot/chromedriver',options=options)
 
 
     ### Login
@@ -46,35 +43,65 @@ def get_balance():
 
     element = browser.find_element(By.XPATH,'/html/body/div[3]/div[1]/span[2]/a').click()
     element = browser.find_element(By.XPATH,'//*[@id="accounts_summary"]/div[3]/div[1]/div/ul/li/div/div/div[2]/div[1]')
-    print(element.text)
     value = element.text
-    element = browser.find_element(By.XPATH, '// *[ @ id = "btn-menu"]').click()
-    time.sleep(0.5)
-    element = browser.find_element(By.XPATH, '// *[ @ id = "lnkFullSite"]').click()
-    time.sleep(1)
-    browser.get("https://client.schwab.com/secure/cc/accounts/positions")
-    time.sleep(1)
-    window_before = browser.window_handles[0]
-    element = browser.find_element(By.XPATH, '//*[@id="exportLink"]').click()
-    window_after = browser.window_handles[1]
-    browser.switch_to.window(window_after)
-    time.sleep(1)
-    element = browser.find_element(By.XPATH, '// *[ @ id = "ctl00_WebPartManager1_wpExportDisclaimer_ExportDisclaimer_btnOk"]').click()
+
+    element = browser.find_element(By.XPATH,'//*[@id="accounts_summary"]/div[3]/div[1]/div/ul/li/div/div/div[1]/div[1]').click()
     time.sleep(5)
+    element = browser.find_element(By.CSS_SELECTOR,'#collapseEquity_7 > li > div > div:nth-child(2) > div.h5.nowrap')
+    cash = element.text
+
+
+    value = float(value.replace("$","").replace(",",""))
+    cash = float(cash.replace("$", "").replace(",",""))
+
+
+    mkt_vl = value - cash
+    print('total: ',value,'market_vl: ',mkt_vl,'cash: ',cash)
+
+    # element = browser.find_element(By.XPATH, '// *[ @ id = "btn-menu"]').click()
+    # time.sleep(0.5)
+    # element = browser.find_element(By.XPATH, '// *[ @ id = "lnkFullSite"]').click()
+    # time.sleep(1)
+    # browser.get("https://client.schwab.com/secure/cc/accounts/positions")
+    # time.sleep(1)
+    #
+    #
+    #
+    # window_before = browser.window_handles[0]
+    # element = browser.find_element(By.XPATH, '//*[@id="exportLink"]').click()
+    # window_after = browser.window_handles[1]
+    # browser.switch_to.window(window_after)
+    # time.sleep(1)
+    # element = browser.find_element(By.XPATH, '// *[ @ id = "ctl00_WebPartManager1_wpExportDisclaimer_ExportDisclaimer_btnOk"]').click()
+    # time.sleep(5)
     print("done")
 
     browser.quit()
-    return value
+    return value, mkt_vl,cash
 value = get_balance()
-fields=[now,value]
+print(value)
+fields2=[now,value[0],1,int('54816757'),value[1],value[2],'USD',]
 
+# fields=[now,float('10000.05'),int('1'),int('54816757'),float('10000.05'),float('5000.05'),'USD']
+print(fields2)
 PATH=os.path.join(path,'portfolio_value.csv')
 
-def line_prepender(filename):
-    with open(filename, 'r+') as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(str(fields[0])+','+str(fields[1]).replace(',','').strip('$') + '\n' + content)
+def line_prepender(filename,fields):
+
+
+    df = pd.DataFrame.from_csv(os.path.join(config.db_path, 'mysql_db_csv/portfolio_value/','54816757.csv'))
+    print(df.tail())
+    new_row=pd.DataFrame([[fields[0],fields[1],fields[2],fields[3],fields[4],fields[5],fields[6]]],columns=['Date','total_value','broker_id','account_number','portfolio_value','cash','currency'])
+    new_row=new_row.reset_index(drop=True)
+    df=df.reset_index()
+    print(new_row)
+    df=df.append(new_row.reset_index(drop=True))
+
+    df=df.reset_index(drop=True)
+    df['entry']=0
+    print(df.tail(10))
+    df.to_csv(os.path.join(config.db_path, 'mysql_db_csv/portfolio_value/','54816757.csv'), index=False)
+
 
 
 def file_mover(download_folder,destination):
@@ -83,8 +110,12 @@ def file_mover(download_folder,destination):
     latest_file = max(list_of_files, key=os.path.getctime)
     name = latest_file.split('/')[-1]
     shutil.move(latest_file, os.path.join(destination,name))
-# line_prepender(PATH,fields)
-file_mover('/Users/joanarau-schweizer/Downloads',path)
+
+
+if __name__=="__main__":
+    # line_prepender(PATH,fields)
+    line_prepender(PATH,fields2)
+    # file_mover('/Users/joanarau-schweizer/Downloads',path)
 
 
 
---
+++ sql_portfoliovalue.py
@@ -0,0 +1,120 @@
+import config
+
+# ssh variables
+host = config.sqldb_ssh
+
+# database variables
+user = config.sqldb_superuser['id']
+password = config.sqldb_superuser['secret']
+db_name = 'brokers_master'
+db_folder = config.db_path
+
+from sqlalchemy import exc
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+pd.set_option('display.max_columns', 500)
+
+
+def get_different_rows(source_df, new_df):
+    print(new_df.tail())
+    print(source_df.tail())
+    """Returns just the rows from the new dataframe that differ from the source dataframe"""
+    if len(new_df) > len(source_df):
+        longer_df = new_df
+        short_df = source_df
+    else:
+        longer_df = source_df
+        short_df = new_df
+    new_rows = []
+    for i in range(len(longer_df)):
+        d1 = longer_df['Date'].iloc[i]
+        try:
+            d2 = short_df['Date'].iloc[i]
+        except:
+            d2 = False
+
+        if d2 == False:
+            new_rows.append(longer_df.iloc[i])
+            print('df1:', d1, 'df2:', d2)
+    print(new_rows)
+    return new_rows
+
+
+def append_portfolio_values_sql(accountnumber):
+    df = pd.DataFrame.from_csv(os.path.join(db_folder, 'mysql_db_csv/portfolio_value/', str(accountnumber + '.csv')))
+    df.rename(columns={'Date': 'Date'})
+    df['account_number'] = accountnumber
+
+    # con = mysql.connector.connect(user=user, password=password, host=host, port='3307', db=db_name)
+    engine = create_engine('mysql+pymysql://' + user + ':' + password + '@' + host + ':' + '3307' + '/' + db_name,
+                           echo=False)
+    con = engine.connect()
+
+    df_old = pd.read_sql(sql='portfolio_value', con=con, index_col='entry', parse_dates='Date')
+    df_old = df_old.reset_index(drop=True)
+    df_old = df_old.loc[df_old['account_number'] == int(accountnumber)]
+    df = df.reset_index(drop=False)
+
+    new_rows = get_different_rows(df_old, df)
+    print(new_rows)
+    # new_rows = df
+    num_rows = len(new_rows)
+    dub = 0
+    for i in range(num_rows):
+
+        try:
+            # Try inserting the row
+            row = pd.DataFrame(new_rows[i]).T
+            print(row)
+            row['Date'] = pd.to_datetime(row['Date'])
+            row['cash'] = float(row['cash'])
+            row['portfolio_value'] = float(row['portfolio_value'])
+            row['total_value'] = float(row['total_value'])
+            row['account_number'] = int(row['account_number'])
+            row['broker_id'] = int(row['broker_id'])
+            row['entry'] = int(0)
+
+            row.to_sql(con=con, name='portfolio_value', index=False, if_exists='append', schema=db_name)
+
+        except exc.IntegrityError:
+            #     # Ignore duplicates
+            print('error')
+            dub += 1
+            pass
+
+    con.close()
+
+
+def update_local_portfoliovalue_csv_fromsql(accountnumber):
+    engine = create_engine('mysql+pymysql://' + user + ':' + password + '@' + host + ':' + '3307' + '/' + db_name,
+                           echo=False)
+    con = engine.connect()
+    # print(con.get_server_info())
+    df_old = pd.read_sql(sql='portfolio_value', con=con, index_col='entry', parse_dates='Date')
+    df_old = df_old.reset_index(drop=True)
+    df_old = df_old.loc[df_old['account_number'] == int(accountnumber)]
+
+    '''Write CSV'''
+    df_old.to_csv(os.path.join(db_folder, 'mysql_db_csv/portfolio_value/', str(accountnumber + '.csv')), index=False)
+    print(df_old.tail())
+    con.close()
+
+
+def clean_local_csv(accountnumber):
+    df = pd.DataFrame.from_csv(os.path.join(db_folder, 'mysql_db_csv/portfolio_value/', str(accountnumber + '.csv')))
+
+    print(df.head())
+    df['total_value'] = df['total_value'].map(lambda x: x.lstrip('$').rstrip(''))
+    df['total_value'] = df['total_value'].str.replace(",", "").astype(float)
+    print(df.tail())
+    df['total_value'] = df['total_value'].astype(float)
+    df.reset_index(drop=False)
+
+    print(df.tail())
+    df.to_csv(os.path.join(db_folder, 'mysql_db_csv/portfolio_value/', str(accountnumber + '.csv')))
+
+
+append_portfolio_values_sql('54816757')
+# update_local_portfoliovalue_csv_fromsql('54816757')
